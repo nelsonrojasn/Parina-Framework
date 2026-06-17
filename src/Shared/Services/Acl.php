56267c222340751b285b6a44e6006577a1a2bc87
Nelson Rojas <nelson_rojas_n@hotmail.com>
2026-06-17 17:06:10 -0400

    Feat: add logger for ACL

--- src/Shared/Services/Acl.php
+++ src/Shared/Services/Acl.php
@@ -4,11 +4,13 @@ namespace Parina\Shared\Services;
 
 use Parina\Shared\Infrastructure\DB;
 use Parina\Core\Config;
+use Parina\Core\FileLogger;
 
 class Acl
 {
     public static function hasPermissions(string $action):bool
     {
+        FileLogger::log("Checking permissions for action: $action");
         return true;
     }
 }
