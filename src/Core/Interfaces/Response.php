9b5b196700eaae147b48a3d22c6b1ac822641482
Nelson Rojas <nelson_rojas_n@hotmail.com>
2026-07-07 09:27:47 -0400

    refactor: clean up Response interface constructor signature to comply with LSP

--- src/Core/Interfaces/Response.php
+++ src/Core/Interfaces/Response.php
@@ -3,10 +3,7 @@ namespace Parina\Core\Interfaces;
 
 interface Response
 {
-    public function __construct(string $content, int $status);
-
     public function getStatus(): int;
     public function getContent(): string;
     public function getHeaders(): array;
-
 }
